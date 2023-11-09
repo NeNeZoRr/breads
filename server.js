b617d3226c1ed60cfd62b440c01141f7a926d3de
NeNeZoRr <zorrillalin@gmail.com>
2023-11-09 12:23:12 -0500

    update7

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose');
+
+
+
+
+
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -7,9 +14,16 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+
+
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -20,7 +34,14 @@ app.get('/', (req, res) => {
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})

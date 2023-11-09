e935e814ef79005dd93b57e81fcf604441711b22
NeNeZoRr <zorrillalin@gmail.com>
2023-11-09 12:20:34 -0500

    updatepart7

--- touch server.js
+++ touch server.js
@@ -1,8 +1,3 @@
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
 // DEPENDENCIES
 const express = require('express')
 
@@ -10,6 +5,11 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,12 +1,41 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index () {
+function Index ({breads, title})  {
     return (
-      <Default>
+        <Default title={title}>
         <h2>Index Page</h2>
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        {/* This is a JSX comment. */}
+        <ul>
+        <Default>
+  <ul>
+  {
+  breads.map((bread, index)=> {
+    return (
+      <li key={index}>
+        <a href={`/breads/${index}`}>
+          {bread.name}
+        </a>
+      </li>
+    )
+  })
+}
+  </ul>
+  <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+
+<div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
+
+</Default>
+        </ul>
       </Default>
+      
     )
 }
+
 
 module.exports = Index
